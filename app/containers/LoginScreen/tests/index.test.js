f0904f8cac5191592cc735cc3a7a8b2310f48d50
mishrpranav007 <59594377+mishrpranav007@users.noreply.github.com>
2020-04-04 00:26:28 +0530

    Added tests for LoginScreen component

--- app/containers/LoginScreen/tests/index.test.js
+++ app/containers/LoginScreen/tests/index.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { fireEvent } from '@testing-library/react-native';
 import { renderWithIntl } from 'app/utils/testUtils';
 import LoginScreen from '../index';
 
@@ -8,8 +9,31 @@ describe('<LoginScreen />', () => {
     expect(baseElement).toMatchSnapshot();
   });
 
-  it('should contain 2 textinput component inside the Loginscreen', () => {
+  it('should contain the 2 textinput component inside the Loginscreen', () => {
     const { getAllByTestId } = renderWithIntl(<LoginScreen />);
     expect(getAllByTestId('log-in-screen-text-input').length).toBe(2);
+  });
+
+  it('should have correct email value paased to it', () => {
+    const emailText = 'abc123@gmail.com';
+    const { getAllByTestId } = renderWithIntl(<LoginScreen />);
+    fireEvent.changeText(
+      getAllByTestId('log-in-screen-text-input')[0],
+      emailText
+    );
+    expect(getAllByTestId('log-in-screen-text-input')[0].props.value).toMatch(
+      emailText
+    );
+  });
+  it('should have correct password value paased to it', () => {
+    const passwordText = 'qwerty123';
+    const { getAllByTestId } = renderWithIntl(<LoginScreen />);
+    fireEvent.changeText(
+      getAllByTestId('log-in-screen-text-input')[1],
+      passwordText
+    );
+    expect(getAllByTestId('log-in-screen-text-input')[1].props.value).toMatch(
+      passwordText
+    );
   });
 });
