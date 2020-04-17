401d6830be9b1409df92af3c7eaae63296c43ca4
mishrpranav007 <59594377+mishrpranav007@users.noreply.github.com>
2020-04-17 18:55:15 +0530

    Initial commit and fixing issues in develop branch

--- android/app/src/main/java/com/reactnativetemplate/MainApplication.java
+++ android/app/src/main/java/com/reactnativetemplate/MainApplication.java
@@ -2,19 +2,9 @@ package com.reactnativetemplate;
 
 import android.app.Application;
 
-import android.util.Log;
 import com.facebook.react.PackageList;
-import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
-import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
-
-import com.reactnativecommunity.rnpermissions.RNPermissionsPackage;
-import org.reactnative.camera.RNCameraPackage;
-import com.imagepicker.ImagePickerPackage;
-import com.reactnativecommunity.rnpermissions.RNPermissionsPackage;
-import org.reactnative.camera.RNCameraPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
+import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
